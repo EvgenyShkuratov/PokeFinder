d550d3fa7b50a7f0124a4162af48093f60ee9731
Evgeny <eebox@mail.ru>
2016-12-17 01:14:02 -0800

    First commit

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Evgeny Shkuratov on 11/22/16.
+//  Copyright © 2016 Evgeny Shkuratov. All rights reserved.
+//
+
+#import "GeoFire.h"
